4b02b6035a34976e5be80c225089abb5beae82bd
gavfu <gavfu@outlook.com>
2024-05-09 13:33:31 +0800

    Post blob tx with multiple blobs and metadata

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import { privateKeyToAccount } from 'viem/accounts';
 // import { mainnetTrustedSetupPath } from 'viem/node';
 import { sepolia, mainnet } from 'viem/chains';
 
+const fs = require('fs');
 const mainnetTrustedSetupPath = './src/trusted-setups/viem-mainnet.json';
 
 async function expBlobs() {
@@ -62,23 +63,47 @@ async function expBlobsSignAndSend() {
   const blobBaseFee = await publicClient.getBlobBaseFee();
   console.log(`Blob base fee: ${blobBaseFee}`);
 
-  const kzg = setupKzg(cKzg, mainnetTrustedSetupPath);
-  const blobs = toBlobs({ data: stringToHex('hello world') });
   const client = createWalletClient({
     account,
     chain: sepolia,
     transport: http(`https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID || ''}`)
   });
+  const kzg = setupKzg(cKzg, mainnetTrustedSetupPath);
+
+  const textBlobs = toBlobs({ data: stringToHex('Data availability sampling: each node only needs to download a small portion of the data to verify the availability of the whole thing.') });
+  const imgContentBase64 = fs.readFileSync('./src/imgs/das.png').toString('base64');
+  const imaData = `data:image/png;base64,${imgContentBase64}`;
+  const imgBlobs = toBlobs({ data: stringToHex(imaData) });
+  // console.log(imaData);
+  // console.log(imgBlobs.length);
+
+  const blobMetadata = {
+    originator: "Vitalik Buterin",
+    description: "An illustration of data availability sampling",
+    blobs: [
+      {
+        content_type: "text/plain",
+        description: "Descritpion of the illustration"
+      },
+      {
+        content_type: "image/png",
+        description: "The illustration image data in base64 format"
+      },
+    ]
+  };
+  const blobMetadataString = JSON.stringify(blobMetadata);
+  const data = stringToHex(blobMetadataString);
+  console.log(data);
 
   const request = await client.prepareTransactionRequest({
     account,
     chain: sepolia,
-    blobs,
+    blobs: [textBlobs[0], imgBlobs[0]],
     kzg,
     to: '0x0000000000000000000000000000000000000000',
     maxFeePerBlobGas: blobBaseFee * 120n / 100n,
     maxPriorityFeePerGas: maxPriorityFeePerGas * 120n / 100n,
-    // data: '0xaaaa'
+    data
   });
   const serializedTransaction = await client.signTransaction(request);
 
